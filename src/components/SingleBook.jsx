6239ad019fc51e9f0b02ad0e6561c0d9b8f48f68
Francesco-Zac <francescozaccaro2@gmail.com>
2025-05-19 16:19:17 +0200

    U3-W10-D1

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-function SingleBook({ book, isSelected, onSelect }) {
+function SingleBook({ book, selected, onBookSelect }) {
+  const handleClick = () => {
+    onBookSelect(selected ? null : book.asin);
+  };
+
   return (
-    <Card className={`h-100 ${isSelected ? "border border-danger" : ""}`} style={{ cursor: "pointer" }} onClick={() => onSelect(book.asin)}>
+    <Card className={`h-100 ${selected ? "border border-danger shadow" : ""}`} onClick={handleClick} style={{ cursor: "pointer" }}>
       <Card.Img variant="top" src={book.img} alt={`Cover di ${book.title}`} />
       <Card.Body>
         <Card.Title className="text-center">{book.title}</Card.Title>
